d5a69a701da41c18df2590c53d49c02eb6075210
CMGYWG <bhartal@gmail.com>
2023-05-03 17:42:50 +0200

    add description to CRUD methods for student

--- src/main/java/com/example/vizsgaremek/controller/StudentController.java
+++ src/main/java/com/example/vizsgaremek/controller/StudentController.java
@@ -3,6 +3,8 @@ package com.example.vizsgaremek.controller;
 import com.example.vizsgaremek.model.Student;
 import com.example.vizsgaremek.model.dto.StudentDTO;
 import com.example.vizsgaremek.service.StudentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,26 +19,60 @@ public class StudentController {
         this.studentService = studentService;
     }
 
+    @Operation(
+            summary = "Find all students",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Responds with a list of Student entities."),
+            }
+    )
     @GetMapping
     public List<Student> findAll() {
         return studentService.findAll();
     }
 
+    @Operation(
+            summary = "Post a student",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Student has been successfully posted."),
+                    @ApiResponse(responseCode = "400", description = "Wrong student object has been posted.")
+            }
+    )
     @PostMapping
     public void save(@Valid @RequestBody StudentDTO studentDTO) {
         studentService.save(studentDTO);
     }
 
+    @Operation(
+            summary = "Find student by id",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Student has been found."),
+                    @ApiResponse(responseCode = "500", description = "No value present.")
+            }
+    )
     @GetMapping("/{id}")
     public Student findById(@PathVariable("id") Long id) {
         return studentService.findById(id);
     }
 
+    @Operation(
+            summary = "Delete student by id",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Student has been deleted."),
+                    @ApiResponse(responseCode = "500", description = "No value present.")
+            }
+    )
     @DeleteMapping("/{id}")
     public void deleteById(@PathVariable("id") Long id) {
         studentService.deleteById(id);
     }
 
+    @Operation(
+            summary = "Update student by id",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Student has been updated."),
+                    @ApiResponse(responseCode = "500", description = "No value present.")
+            }
+    )
     @PutMapping("/{id}")
     public void update(@PathVariable("id") Long id,@Valid @RequestBody StudentDTO studentDTO) {
         studentService.update(id, studentDTO);
