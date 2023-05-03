e3c52212501ec7a23644a92a2846352a877451a5
CMGYWG <bhartal@gmail.com>
2023-05-03 17:32:20 +0200

    add description to CRUD methods for mentor

--- src/main/java/com/example/vizsgaremek/controller/MentorController.java
+++ src/main/java/com/example/vizsgaremek/controller/MentorController.java
@@ -3,6 +3,8 @@ package com.example.vizsgaremek.controller;
 import com.example.vizsgaremek.model.Mentor;
 import com.example.vizsgaremek.model.dto.MentorDTO;
 import com.example.vizsgaremek.service.MentorService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,27 +18,60 @@ public class MentorController {
     public MentorController(MentorService mentorService) {
         this.mentorService = mentorService;
     }
-
+    @Operation(
+            summary = "Find all mentors",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Responds with a list of Mentor entities."),
+            }
+    )
     @GetMapping
     public List<Mentor> findAll() {
         return mentorService.findAll();
     }
 
+    @Operation(
+            summary = "Post a mentor",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Mentor has been successfully posted."),
+                    @ApiResponse(responseCode = "400", description = "Wrong mentor object has been posted.")
+            }
+    )
     @PostMapping
     public void save(@Valid @RequestBody MentorDTO mentorDTO) {
         mentorService.save(mentorDTO);
     }
 
+    @Operation(
+            summary = "Find mentor by id",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Mentor has been found."),
+                    @ApiResponse(responseCode = "500", description = "No value present.")
+            }
+    )
     @GetMapping("/{id}")
     public Mentor findById(@PathVariable("id") Long id) {
         return mentorService.findById(id);
     }
 
+    @Operation(
+            summary = "Delete mentor by id",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Mentor has been deleted."),
+                    @ApiResponse(responseCode = "500", description = "No value present.")
+            }
+    )
     @DeleteMapping("/{id}")
     public void deleteById(@PathVariable("id") Long id) {
         mentorService.deleteById(id);
     }
 
+    @Operation(
+            summary = "Update mentor by id",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Mentor has been updated."),
+                    @ApiResponse(responseCode = "500", description = "No value present.")
+            }
+    )
     @PutMapping("/{id}")
     public void update(@PathVariable("id") Long id,@Valid @RequestBody MentorDTO mentorDTO) {
         mentorService.update(id, mentorDTO);
